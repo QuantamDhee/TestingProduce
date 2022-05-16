6cf2be1ac7ca0536ddcd6d014e010b84cc61742e
Johndhee Ballesteros <Johndheeballesteros13@gmail.com>
2022-05-16 07:23:16 -0700

    added navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navigation() {
+    return(
+        <div>
+            <h3>This is the navigation</h3>
+        </div>
+    )
+}
+
+export default Navigation
