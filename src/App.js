49291d4409ba954ec43fcd305f1d54cfe22be628
Johndhee Ballesteros <Johndheeballesteros13@gmail.com>
2022-05-16 17:52:34 -0700

    browserRouter, Routes, route in app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import "./App.css";
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Header from "./components/Header"
 import Navigation from "./components/Navigation";
+import Home from "./paths/Home"
+import About from "./paths/About";
+import Contact from "./paths/Contact";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Navigation />
+      <BrowserRouter>
+        <Header />
+        <Navigation />
+        <main className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </main>
+      </BrowserRouter>
+      
     </div>
   );
 }
