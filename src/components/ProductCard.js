b150581cd4c91788fbf247e85261155a9c7728ed
Johndhee Ballesteros <Johndheeballesteros13@gmail.com>
2022-05-21 06:31:26 -0700

    trying out mui for productCard

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,11 +1,69 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import { Card, Image } from "semantic-ui-react"
+// import React from 'react'
 
-function ProductCard() {
+// function ProductCard() {
+//   return (
+//     <div>ProductCard</div>
+//   )
+// }
+
+// export default ProductCard
+import React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia'
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { ClassNames } from '@emotion/react';
+
+// const bull = (
+//   <Box
+//     component="span"
+//     sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+//   >
+//     •
+//   </Box>
+// );
+
+function ProductCard({product}) {
   return (
-    <div>ProductCard</div>
-  )
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image='' title={product.name}  />
+      <CardContent>
+        <div className={classes.cardContent} >
+          <Typography variant="h5" gutterBottom >
+            {product.name}
+          </Typography>
+          <Typography variant="h5">
+            {product.price}
+          </Typography>
+        </div>
+        <Typography variant="h2" color="textSecondary">
+        </Typography>
+      </CardContent>
+      {/* <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions> */}
+    </Card>
+  );
 }
+
 
 export default ProductCard
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,40 +1,51 @@
 import React from 'react'
+import Grid from "@mui/material/Grid"
 import { useState } from 'react'
+import ProductCard from './ProductCard'
 
 function Products() {
     const [products, setProducts] = useState([
         {
             id: 1, 
             name: "Garlic",
-            price: "3.00"
+            price: "$3.00"
         },
         {
             id: 2, 
             name: "Onion",
-            price: "3.50"
+            price: "$3.50"
         },
         {
             id: 3, 
             name: "Asparagus",
-            price: "2.50"
+            price: "$2.50"
         },
         {
             id: 4, 
             name: "BitterMelon",
-            price: "4.00"
+            price: "$4.00"
         },
         {
             id: 5, 
             name: "Okra",
-            price: "5.00"
+            price: "$5.00"
         },
     ])
 
   return (
+    // <div>
+    //     {products.map((product) => (
+    //         <h3 key={product.id}>{product.name}</h3>
+    //     ))}
+    // </div>
     <div>
-        {products.map((product) => (
-            <h3 key={product.id}>{product.name}</h3>
-        ))}
+        <Grid container spacing={4} >
+            {products.map((product) => (
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <ProductCard product={product}/>
+                </Grid>
+            ))}
+        </Grid>
     </div>
   )
 }
